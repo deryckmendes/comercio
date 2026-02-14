3d3da9e5958a8624dc096c4f343635944c5def63
ezequiel resende <deryckvinicius098@gmail.com>
2026-02-14 14:22:57 -0300

    feat(catalogo): Spring Application Created

---
+++ comercio-catalogo/src/main/java/com/comercio/comercio_catalogo/ComercioCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.comercio.comercio_catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioCatalogoApplication.class, args);
+	}
+
+}
---
+++ comercio-catalogo/src/test/java/com/comercio/comercio_catalogo/ComercioCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comercio.comercio_catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
