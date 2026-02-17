cf89b19276f4f0e62e8c5aa449f1db37e9d299e0
Deryck Mendes <deryckvinicius098@gmail.com>
2026-02-17 16:14:36 -0300

    feat(auth): initialize SpringBoot application for system authentication

---
+++ comercio_auth/src/main/java/com/comercio/comercio_auth/ComercioAuthApplication.java
@@ -0,0 +1,13 @@
+package com.comercio.comercio_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioAuthApplication.class, args);
+	}
+
+}
---
+++ comercio_auth/src/test/java/com/comercio/comercio_auth/ComercioAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comercio.comercio_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
